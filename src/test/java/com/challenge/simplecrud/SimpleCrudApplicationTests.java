5db5a85844ae430f847509c7342fa530c77288be
Bruno H. Homem <brunohhomem@gmail.com>
2024-01-01 15:39:23 -0300

    first commit

---
+++ src/test/java/com/challenge/simplecrud/SimpleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.simplecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
